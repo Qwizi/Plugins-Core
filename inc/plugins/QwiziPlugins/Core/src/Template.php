f460ca8b64c99bea91aea5e222940647b3b04c35
Adrian Ciołek <qwizi95@protonmail.com>
2020-06-23 23:42:48 +0200

    Remved version classes and add method to Template class

--- inc/plugins/QwiziPlugins/Core/src/Template.php
+++ inc/plugins/QwiziPlugins/Core/src/Template.php
@@ -4,8 +4,24 @@ declare(strict_types=1);
 
 namespace Qwizi\Core;
 
+use \DirectoryIterator;
+
 class Template
 {
+    public static function getTemplates(string $path)
+    {
+        $templates = [];
+        $path .= '/templates/';
+
+        foreach (new DirectoryIterator($path) as $file) {
+            if ($file->isDot() || $file->getExtension() !== 'html' || $file->getFilename() === 'index.html') continue;
+            $templateName = pathinfo($file->getFilename())['filename'];
+            $templateContent = \file_get_contents($file->getPathname());
+            $templates[$templateName] = $templateContent;
+        }
+
+        return $templates;
+    }
     public static function eval($variable, $templateName, bool $many=False)
     {
         global $templates;
